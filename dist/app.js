4d9d7337c5272fd46915b82d0a4b730b16c5ec10
Jay-Tiroh <uchechijethro1@gmail.com>
2024-07-11 23:42:15 +0100

    added history functionality

--- dist/app.js
+++ dist/app.js
@@ -2,19 +2,48 @@
 
 const topOutput = document.querySelector(".calculation");
 const result = document.querySelector(".result");
+const shiftText = document.querySelector(".shift-text");
+const shiftBtn = document.querySelector(".shift-btn");
+const navBtns = document.querySelectorAll(".nav-btn");
 const btns = document.querySelectorAll(".btn");
 const yellowBtns = document.querySelectorAll(".yellow");
 const brackets = document.querySelectorAll(".brace");
+const menuBtn = document.querySelector(".menu-btn");
 
 result.innerHTML = 0;
+
 // VARIABLES
 let evaluated = false;
 let storedInput = [];
 let currentValue;
 let ans;
 let equation;
+let shiftOn = false;
+let history = [];
+let currentIndex;
 
 // EVENT LISTENERS
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    clear(topOutput);
+    result.innerHTML = "0";
+  }, 2000);
+  updateHistory();
+  topOutput.innerHTML = "press SHIFT + AC to clear history";
+  result.innerHTML = "not perfect yet tho";
+});
+
+menuBtn.addEventListener("click", () => {
+  topOutput.innerHTML = "bros abeg later";
+  result.innerHTML = "I don tire";
+
+  setTimeout(() => {
+    clear(topOutput);
+    clear(result);
+  }, 2000);
+});
+
 brackets.forEach((brace) => {
   brace.addEventListener("click", (e) => {
     let symbol = e.currentTarget.dataset.sign;
@@ -33,7 +62,7 @@ btns.forEach((btn) => {
     const thisBtn = e.currentTarget;
     const value = thisBtn.innerHTML;
     let symbol = e.currentTarget.dataset.sign;
-
+    shiftText.classList.add("hidden");
     if (evaluated) {
       clear(topOutput);
       evaluated = false;
@@ -62,20 +91,84 @@ btns.forEach((btn) => {
       clear(topOutput);
       result.innerHTML = 0;
       storedInput.length = 0;
+      currentIndex = history.length - 1;
+      if (shiftOn) clearHistory();
     }
 
     if (thisBtn.classList.contains("delete")) {
       storedInput.pop();
-      // let lastDigit = topOutput.innerHTML.charAt(-1);
       topOutput.innerHTML = topOutput.innerHTML.slice(0, -1);
     }
+
+    // if (thisBtn.classList.contains("shift-btn")) {
+    //   shiftText.classList.remove("hidden");
+    // }
     if (e.currentTarget.classList.contains("equals")) {
       calculate();
+    }
+    shiftOn = false;
+  });
+});
+
+shiftBtn.addEventListener("click", () => {
+  if (shiftText.classList.contains("hidden")) {
+    shiftText.classList.remove("hidden");
+    shiftOn = true;
+  } else {
+    shiftText.classList.add("hidden");
+    shiftOn = false;
+  }
+});
+
+navBtns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    const thisBtn = e.currentTarget;
+    console.log(history);
+    if (history.length > 0) {
+      if (thisBtn.classList.contains("prev") && currentIndex > 0) {
+        if (currentIndex === history.length - 1) {
+          updateDisplay(currentIndex);
+          currentIndex--;
+        } else {
+          currentIndex--;
+          updateDisplay(currentIndex);
+        }
+      }
+      if (
+        thisBtn.classList.contains("next") &&
+        currentIndex < history.length - 1
+      ) {
+        if (currentIndex === history.length - 1) updateDisplay(currentIndex);
+        else {
+          currentIndex++;
+          updateDisplay(currentIndex);
+        }
+      }
+    } else {
+      setTimeout(() => {
+        clear(topOutput);
+        result.innerHTML = "0";
+      }, 2000);
+      clear(result);
+      topOutput.innerHTML = "No previous History";
     }
   });
 });
 
 // FUNCTIONS
+
+function addToLocalStorage(expression, solution) {
+  const item = { expression, solution };
+  let history = getLocalStorage();
+  history.push(item);
+  localStorage.setItem("history", JSON.stringify(history));
+}
+
+function getLocalStorage() {
+  return localStorage.getItem("history")
+    ? JSON.parse(localStorage.getItem("history"))
+    : [];
+}
 
 function calculate() {
   let answer;
@@ -88,6 +181,9 @@ function calculate() {
     answer = eval(equation);
     result.innerHTML = answer;
     ans = answer;
+    addToLocalStorage(equation, ans);
+    history = getLocalStorage();
+    currentIndex = history.length - 1;
   } catch (error) {
     error.message = `calculation error.
     check input.`;
@@ -109,4 +205,19 @@ function calculate() {
 
 function clear(item) {
   item.innerHTML = "";
+}
+
+function clearHistory() {
+  localStorage.clear();
+  history.length = 0;
+}
+
+function updateDisplay(index) {
+  topOutput.innerHTML = history[index].expression;
+  result.innerHTML = history[index].solution;
+}
+
+function updateHistory() {
+  history = getLocalStorage();
+  currentIndex = history.length - 1;
 }
